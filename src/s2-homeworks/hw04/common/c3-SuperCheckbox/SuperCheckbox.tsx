1b0aec8ae7f71ba2ee78dea581afb1bf031c610e
Polina Sedunova <lina.malina.spb@gmail.com>
2022-11-01 17:29:17 +0300

    refactor onChangeCallback

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,11 +27,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        if (onChangeChecked) {
-            onChangeChecked(e.currentTarget.checked)
-        }
+        onChange && onChange(e)
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)
          // задачка на написание онченджа
-
     }
 
     const finalInputClassName = s.checkbox
