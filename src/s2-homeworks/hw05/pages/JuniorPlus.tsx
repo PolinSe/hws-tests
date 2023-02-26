6f1829f42a2dd2895ffca75a7af343c8d1ab504b
Polina Sedunova <lina.malina.spb@gmail.com>
2023-02-26 17:19:58 +0300

    hw12 solution

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import HW10 from '../../hw10/HW10'
-import HW11 from '../../hw11/HW11';
-// import HW12 from '../../hw12/HW12'
-// import HW13 from '../../hw13/HW13'
+import HW11 from '../../hw11/HW11'
+import HW12 from '../../hw12/HW12'
+import {Provider} from 'react-redux';
+import store from '../../hw10/bll/store';
+//import HW13 from '../../hw13/HW13'
 
 function JuniorPlus() {
     return (
         <div id={'hw5-page-junior-plus'}>
             <HW10 />
             <HW11 />
-            {/*<HW12 />*/}
+            <Provider store={store}>
+                <HW12 />
+            </Provider>
+
             {/*<HW13 />*/}
         </div>
     )
--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -3,7 +3,10 @@ import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import {changeThemeId, StateType} from './bll/themeReducer'
+import {AppRootStateType} from './bll/store';
+
+
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -12,18 +15,27 @@ import {changeThemeId} from './bll/themeReducer'
 * 4 - передать пропсы в SuperSelect
 * */
 
-const themes = [
+export type themeType = {
+    id: number
+    value: string
+}
+const themes: themeType[] = [
     {id: 1, value: 'light'},
     {id: 2, value: 'blue'},
     {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
+
+
     // взять ид темы из редакса
-    const themeId = 1
 
-    const change = (id: any) => { // дописать функцию
+    const themeId = useSelector<AppRootStateType, number>(state => state.theme.themeId)
 
+    const dispatch = useDispatch()
+
+    const change = (id: number) => { // дописать функцию
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -40,6 +52,9 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
+                    value={themeId}
+                    options={themes}
+                    onChangeOption={change}
                     // сделать переключение тем
 
                 />
